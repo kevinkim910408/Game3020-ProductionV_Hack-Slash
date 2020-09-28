07026188597e9a8ffa872f1f2f12d6d03fe673a0
kevinkim0408 <kevinkim0408@naver.com>
2020-09-28 16:37:47 +0900

    Movement Work
    1. add jump
    2. add dash
    3. prevent double jump using layers

--- Assets/RigidBodyCharater.cs
+++ Assets/RigidBodyCharater.cs
@@ -9,6 +9,8 @@ using UnityEngine;
 /// History: 
 /// 2020-09-28  - add variables about movements.
 ///             - 
+///             
+/// Last Modified: 2020-09-28
 /// </summary>
 
 public class RigidBodyCharater : MonoBehaviour
@@ -24,8 +26,12 @@ public class RigidBodyCharater : MonoBehaviour
 
     //movement(vector3)
     private Vector3 inputDirection = Vector3.zero;
-    #endregion
 
+    //prevent double jump
+    private bool isGround = false;
+    public LayerMask groundLayerMask;
+    public float groundCheckDistance = 0.3f;
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -37,7 +43,9 @@ public class RigidBodyCharater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //user input
+        CheckGroundStatus();
+
+        //user input - basic movments
         inputDirection = Vector3.zero;
         inputDirection.x = Input.GetAxis("Horizontal");
         inputDirection.z = Input.GetAxis("Vertical");
@@ -47,6 +55,21 @@ public class RigidBodyCharater : MonoBehaviour
         { 
             transform.forward = inputDirection;
         }
+
+        //user input - jump
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(jumpHeight * -2.0f * Physics.gravity.y);
+            rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+        }
+
+        //user input - dash
+        if (Input.GetButtonDown("Dash"))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime), 
+                0, (Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime)));
+            rigidbody.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
     }
 
     void FixedUpdate()
@@ -54,5 +77,20 @@ public class RigidBodyCharater : MonoBehaviour
         rigidbody.MovePosition(rigidbody.position + inputDirection * speed * Time.fixedDeltaTime);
     }
 
+    #region Methods
+    //to check if player is on the ground
+    void CheckGroundStatus()
+    {
+        RaycastHit raycastHit;
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out raycastHit, groundCheckDistance, groundLayerMask))
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+    }
+    #endregion Methods
 
 }
