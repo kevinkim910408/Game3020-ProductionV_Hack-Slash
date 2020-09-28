02aa390f631906c2797f1fda2194730ef474dbe7
kevinkim0408 <kevinkim0408@naver.com>
2020-09-28 21:17:25 +0900

    Camera Work
    Follow Player - top down

---
+++ Assets/Scripts/TopDownCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    #region Variables
+
+    //Basic variables for camera
+    [SerializeField]
+    float height = 5f;
+    [SerializeField]
+    float distance = 10f;
+    [SerializeField]
+    float angle = 45f;
+    [SerializeField]
+    float lookAtHeight = 2f;
+    [SerializeField]
+    float smoothSpeed = 0.5f;
+
+    //target = player
+    [SerializeField]
+    Transform target;
+
+    private Vector3 refVelocity;
+    #endregion
+
+    //LateUpdate is called after all Update functions have been called.
+    void LateUpdate()
+    {
+        HandleCamera();
+    }
+
+    [SerializeField]
+    public void HandleCamera()
+    {
+        if (!target)
+        {
+            return;
+        }
+
+        // Build world position vector
+        Vector3 worldPosition = (Vector3.forward * -distance) + (Vector3.up * height);
+
+        // Build Rotated vector
+        Vector3 rotatedVector = Quaternion.AngleAxis(angle, Vector3.up) * worldPosition;
+
+        // Move player position
+        Vector3 flatTargetPosition = target.position;
+        flatTargetPosition.y += lookAtHeight;
+
+        Vector3 finalPosition = flatTargetPosition + rotatedVector;
+
+        transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, smoothSpeed);
+
+        transform.LookAt(target.position);
+    }
+}
